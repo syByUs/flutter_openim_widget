6f39df1be9d55dc83641075c37357b511e062e44
PaulPan <panhaohua2014@yahoo.com>
2022-12-02 10:59:07 +0800

    更改ChatBubble的margin padding

--- lib/src/chat_bubble.dart
+++ lib/src/chat_bubble.dart
@@ -12,24 +12,29 @@ class ChatBubble extends StatelessWidget {
     this.constraints,
     this.backgroundColor,
     this.child,
+    this.decoration,
+    this.margin,
+    this.padding,
     required this.bubbleType,
   }) : super(key: key);
   final BoxConstraints? constraints;
   final Color? backgroundColor;
   final Widget? child;
   final BubbleType bubbleType;
-
+  final EdgeInsets? margin;
+  final EdgeInsets? padding;
+  final BoxDecoration? decoration;
   @override
   Widget build(BuildContext context) {
     return Container(
       constraints: constraints,
-      margin: EdgeInsets.only(right: 10.w, left: 10.w, bottom: 2.h),
-      padding: EdgeInsets.symmetric(
-        horizontal: 7.w,
-        vertical: 7.h,
+      margin: margin ?? EdgeInsets.only(right: 10.w, left: 10.w, bottom: 2.h),
+      padding: padding ?? EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 8,
       ),
       alignment: Alignment.center,
-      decoration: BoxDecoration(
+      decoration: decoration ?? BoxDecoration(
         color: backgroundColor,
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(bubbleType == BubbleType.send ? 8 : 1),
